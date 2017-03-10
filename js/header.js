ac9cfc91b16afd5cc3496941afac2bc5b90e0bad
kevinli96 <kerui_li@brown.edu>
2017-03-10 13:17:54 -0500

    embedded iframes

--- js/header.js
+++ js/header.js
@@ -1,8 +1,20 @@
 $(window).on("load", function() {
     $.get("header.html", function(data) {
-      $('body').prepend(data);
+        $('body').prepend(data);
     });
     $('.carousel').carousel({
         interval: 5000 //changes the speed
     })
+
+    function scrollToAnchor() {
+        var title = $(".title");
+        if (title.length > 0) {
+        $('html,body').animate({
+            scrollTop: title.offset().top + 250
+        }, 'slow');
+      }
+    }
+
+    scrollToAnchor();
+
 });
