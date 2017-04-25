4df37bfdd8c096f90775f78d148c93a09b36863a
kevinli96 <kerui_li@brown.edu>
2017-04-24 21:08:56 -0400

    more team changes

--- js/team.js
+++ js/team.js
@@ -3,6 +3,15 @@ const TEAM_SIZE = 3;
 let loadTeam = (teamNo) => {
   $.get("teams/team" + teamNo + ".html", function(data) {
       $('.team' + teamNo).html(data);
+
+      $("td").filter(function() {
+        return $(this).text() === "A";
+      }).css( "background-color", "#db9d97" );
+      $("td").filter(function() {
+        return $(this).text() === "H";
+      }).css( "background-color", "#8ec199" );
+
+      $('.team' + teamNo + " > table").addClass("table table-bordered table-hover")
   });
 
 }
@@ -10,4 +19,5 @@ $(window).on("load", function() {
     for (var i = 1; i <= TEAM_SIZE; i++) {
       loadTeam(i);
     }
+
 });
