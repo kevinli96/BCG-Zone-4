6cf63faf76073e1941206acf48e94f9c2c9a5037
kevinli96 <kerui_li@brown.edu>
2017-05-16 20:05:27 -0400

    captains page changes

--- js/login.js
+++ js/login.js
@@ -1,9 +1,14 @@
 $(function() {
-  $('#submitButton').on('click', handleSubmit);
+  $('#loginButton').on('click', handleSubmit);
 });
 
 function handleSubmit() {
   let user = $('#username').val();
   let pass = $('#password').val();
-  
+
+  if (user = "golfgal" && pass == "W0menl0veg0lf") {
+    window.location = "rosters.html";
+  }
+  return false;
+
 }
