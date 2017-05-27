63d0d21ae19c1ef6254dffe9a437941fddd922f6
kevinli96 <kerui_li@brown.edu>
2017-05-26 23:38:05 -0700

    about tab changes

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ function handleSubmit() {
   let user = $('#username').val();
   let pass = $('#password').val();
 
-  if (user = "golfgal" && pass == "W0menl0veg0lf") {
+  if (user === "golfgal" && pass === "W0menl0veg0lf") {
     // window.location = "secure/rosters.html";
     $('#incorrectPass').remove();
   } else {
