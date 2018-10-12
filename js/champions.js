543454ec2b1fa1fd8ea5570fe8d4cb579a771432
SherryZone4 <33670024+sworkca@users.noreply.github.com>
2018-10-11 17:12:48 -0700

    SW fixed underscore

--- js/champions.js
+++ js/champions.js
@@ -8,8 +8,8 @@ function loadAll() {
   })
   .done(loadJunior)
   .done(loadSenior)
-  .done(loadSenior_Z4)
-  .done(load24ball_Z34)
+  .done(loadSeniorZ4)
+  .done(load24ballZ34)
 }
 
 function loadJunior() {
@@ -20,18 +20,18 @@ function loadJunior() {
 
 function loadSenior() {
   $.get("tables/past-champs-senior-Z34.html", function(data) {
-    $(data).appendTo("#seniorChampions_Z34")
+    $(data).appendTo("#seniorChampionsZ34")
   })
 }
 
 function loadSenior_Z4() {
   $.get("tables/past-champs-senior-Z4.html", function(data) {
-    $(data).appendTo("#seniorChampions_Z4")
+    $(data).appendTo("#seniorChampionsZ4")
   })
 }
 
 function load24ball_Z34() {
   $.get("tables/past-champs-24ball-Z34.html", function(data) {
-    $(data).appendTo("#24ball_Z34")
+    $(data).appendTo("#24ballZ34")
   })
 }
