2f0c26f1719b00325c329685d4241b67c7bf3e80
SherryZone4 <33670024+sworkca@users.noreply.github.com>
2018-10-11 17:08:49 -0700

    SW forgot js files

--- js/champions.js
+++ js/champions.js
@@ -20,18 +20,18 @@ function loadJunior() {
 
 function loadSenior() {
   $.get("tables/past-champs-senior-Z34.html", function(data) {
-    $(data).appendTo("#seniorChampions-Z34")
+    $(data).appendTo("#seniorChampions_Z34")
   })
 }
 
 function loadSenior_Z4() {
   $.get("tables/past-champs-senior-Z4.html", function(data) {
-    $(data).appendTo("#seniorChampions-Z4")
+    $(data).appendTo("#seniorChampions_Z4")
   })
 }
 
 function load24ball_Z34() {
   $.get("tables/past-champs-24ball-Z34.html", function(data) {
-    $(data).appendTo("#24ball-Z34")
+    $(data).appendTo("#24ball_Z34")
   })
 }
